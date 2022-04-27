220d022a5733ab6b7bb8bc478510b2695b6e40e4
Guilherme Freitas <95315019+guiFreitasDev1@users.noreply.github.com>
2022-04-27 06:49:45 -0300

    1.4

--- index.js
+++ index.js
@@ -16,3 +16,34 @@ function leiaMais(){
     }
 }
 
+function topico(){
+    var textDiscussoesOff = document.querySelector('.textDiscussoesOff')
+    var enviarTopico = document.querySelector('.enviarTopico')
+    
+    if (enviarTopico.style.display === 'none'){
+        enviarTopico.style.display = 'inline'
+        textDiscussoesOff.style.display = 'none'
+    }else{
+        enviarTopico.style.display = 'none'
+        textDiscussoesOff.style.display = 'inline'
+    }
+
+}
+
+function enviarDisc(){
+    var enviadoNovoTopico = document.querySelector('.enviadoNovoTopico')
+    var enviarTopico = document.querySelector('.enviarTopico')
+    var buttonEnviarTopico = document.querySelector('.buttonEnviarTopico')
+
+    if (enviarTopico.style.display === 'inline'){
+        enviarTopico.style.display = 'none'
+        enviadoNovoTopico.style.display = 'inline'
+        
+    }else{
+        enviarTopico.style.display = 'inline'
+        enviadoNovoTopico.style.display = 'none'
+        
+    }
+
+
+}
